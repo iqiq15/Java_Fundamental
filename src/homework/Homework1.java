267fa59d94f1085c5106fdf889c8a1c7e5f7fe49
iqiq15 <leesy0914@gmail.com>
2019-12-02 18:24:35 +0900

    2019-12-02-v2

--- src/homework/Homework1.java
+++ src/homework/Homework1.java
@@ -14,68 +14,52 @@ public class Homework1 {
 		int[] monthArray = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 		// coding
-		
 
 		int preYear = year - 1;
 		int preMonth = month - 1;
 		int totalCount = 0;
-		//2018년도까지 총 일 수 구하기
+		// 2018년도까지 총 일 수 구하기
 		totalCount = preYear * 365 + (preYear / 4 - preYear / 100 + preYear / 400);
-		//2019년도가 윤년인지 판단한다
+		// 2019년도가 윤년인지 판단한다
 		boolean isLeafYear = year % 4 == 0 && year % 100 == 0 || year % 400 == 0;
-		//2019년도 11월까지 합 구하기
-		if(isLeafYear){
+		// 2019년도 11월까지 합 구하기
+		if (isLeafYear) {
 			monthArray[1] = 29;
 		}
 
 		for (int i = 0; i < preMonth; i++) {
 			totalCount += monthArray[i];
 		}
-		
+
 		totalCount += day;
-		
+
 		int dayOfWeek = totalCount % 7;
-		if(dayOfWeek ==1){
+		if (dayOfWeek == 1) {
 			message = "월요일";
-		}else if(dayOfWeek ==2){
+		} else if (dayOfWeek == 2) {
 			message = "화요일";
-		}else if(dayOfWeek ==3){
+		} else if (dayOfWeek == 3) {
 			message = "수요일";
-		}else if(dayOfWeek ==4){
+		} else if (dayOfWeek == 4) {
 			message = "목요일";
-		}else if(dayOfWeek ==5){
+		} else if (dayOfWeek == 5) {
 			message = "금요일";
-		}else if(dayOfWeek ==6){
+		} else if (dayOfWeek == 6) {
 			message = "토요일";
-		}else if(dayOfWeek ==0){
+		} else if (dayOfWeek == 0) {
 			message = "일요일";
 		}
 		System.out.printf("%d년 %d월 %d일 %s 입니다.", year, month, day, message);
 	}
-	
+
 	public static void main1(String[] args) {
 		System.out.println("일\t월\t화\t수\t목\t금\t토");
 		for (int i = 1; i <= 31; i++) {
 			System.out.println("\t");
-			if(i%7 ==0){
+			if (i % 7 == 0) {
 				System.out.println("\t");
 			}
-			
+
 		}
 	}
 }
-
-/* 내가 만든
- * int preyear = year - 1; int yearDay = preyear * 365 + preyear / 4 -
- * preyear / 100 + preyear / 400;
- * 
- * int monthDay = 0; for (int i = 0; i < month - 1; i++) { monthDay +=
- * monthArray[i]; }
- * 
- * int totalDay = yearDay + monthDay + day; totalDay = totalDay % 7; if
- * (totalDay == 0) { message = "일요일"; } else if (totalDay == 1) {
- * message = "월요일"; } else if (totalDay == 2) { message = "화요일"; } else
- * if (totalDay == 3) { message = "수요일"; } else if (totalDay == 4) {
- * message = "목요일"; } else if (totalDay == 5) { message = "금요일"; } else
- * if (totalDay == 6) { message = "토요일"; }
- */
